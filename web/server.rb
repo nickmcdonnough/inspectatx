e8db32bf1aedbdb770bd999e84441b1e99ccb36c
Nick McDonnough <nickmcd@gmail.com>
2015-04-02 23:40:14 -0500

    Start Sinatra server.

--- web/server.rb
+++ web/server.rb
@@ -1,9 +1,27 @@
 require 'sinatra'
+require 'json'
+require_relative '../lib/restinspect.rb'
 
 class RestInspectServer < Sinatra::Application
+  attr_reader :params
+
+  before '/search' do
+    @params = JSON.parse request.body.read
+  end
 
   get '/' do
     send_file 'index.html'
   end
 
+  post '/search' do
+    results = RestInspect::Lookup.run params
+
+    data = if results.success?
+      RestInspect::PrepResults.run results.data
+    else
+      {}
+    end
+
+    JSON.generate(data)
+  end
 end
