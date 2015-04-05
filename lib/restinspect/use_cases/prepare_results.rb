7f3abdcb64312f8718428eef7246fb5297627073
Nick McDonnough <nickmcd@gmail.com>
2015-04-04 22:11:30 -0500

    Update buildgeojson use case so that it adds all inspections for a facility directly to the geojson structure.

--- lib/restinspect/use_cases/prepare_results.rb
+++ lib/restinspect/use_cases/prepare_results.rb
@@ -1,13 +1,16 @@
 module RestInspect
-  class BuildGeoJSON
+  class BuildGeoJSON < UseCase
     def run results
-      return {results: []} if data.empty?
+      return {results: []} if results.empty?
 
       {
         results: results.map do |r|
           facility = JSON.parse(r.to_json)
+          inspections = r.inspections.map { |x| JSON.parse(x.to_json) }
+
           geojson = JSON.parse(facility.delete('gj'))
           geojson['properties'] = facility
+          geojson['properties']['inspections'] = inspections
           geojson
         end
       }
